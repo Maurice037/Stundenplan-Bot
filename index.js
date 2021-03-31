cc2c368e2ab9dc7cb421f43122e5a8d24e04aedd
Maurice <55408918+Maurice037@users.noreply.github.com>
2021-03-31 18:24:04 +0200

    clear code and code fix

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const cfg = require("./config.js");
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const WebUntis = require("webuntis");
 
 let x = false;
 
-console.log(cfg.school.name)
 client.on("ready", () => {
     console.log("STARTET")
+    client.user.setActivity("COVID-19 PLATIN");
 })
 
 function beginn(time) {
@@ -28,18 +27,16 @@ const untis = new WebUntis(
 untis
     .login()
     .then(() => {
-        return untis.getOwnTimetableFor(new Date("2021-04-12"));
-        //return untis.getOwnTimetableForToday();
+        return untis.getOwnTimetableForToday();
     })
     .then(timetable => {
-        //timetable
+        //check lesson 
         setInterval(function () {
             const now = new Date();
             let time = parseInt(now.getHours() + "" + now.getMinutes());
             let data = timetable.map(x => { return { start: x.startTime, end: x.endTime, fach: x.su[0].longname, raum: x.ro[0].orgname, lehrer: x.te[0].orgname } })
             if (data.some(data => data.start === time) && x === false) {
                 let i = data.findIndex(x => x.start === time);
-                console.log(data[i].name)
                 client.channels.cache.get(cfg.channel).send(`**Unterricht um ${beginn(data[i].start)} Uhr!** \nFach: ${data[i].fach} \nRaum: ${data[i].raum} \nLehrer: ${data[i].lehrer}`);
 
                 x = true
@@ -47,11 +44,11 @@ untis
              if(data.some(data => data.end === time) && x === true ){
                  x = false
              }
-        }, 60000);
+        }, 30000);//check every 30 seconds 
     })
     .catch(err => {
         console.error(err);
     });
 
-
+//connect bot 
 client.login(cfg.token);
